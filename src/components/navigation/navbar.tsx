ee9222e5ae6ec972ac1dca75e8b4fdd78a32abcf
Ayota <seculex4@gmail.com>
2024-12-23 11:20:35 +0530

    New commit

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Icons from "../global/icons";
 import { buttonVariants } from "../ui/button";
 import { currentUser } from "@clerk/nextjs/server";
 import { UserButton } from "@clerk/nextjs";
@@ -9,7 +8,7 @@ export async function Navbar() {
   const user = await currentUser();
 
   return (
-<header className="fixed top-10 px-6 py-4 bg-background/40 backdrop-blur-lg border border-white rounded-2xl shadow-xl z-50 w-full max-w-sm sm:max-w-[440px] md:max-w-4xl mx-auto">
+    <header className="fixed top-10 px-6 py-4 bg-background/40 backdrop-blur-lg border border-white rounded-2xl shadow-xl z-50 w-full max-w-sm sm:max-w-[440px] md:max-w-4xl mx-auto">
       <div className="flex items-center justify-between h-full">
         <div className="flex items-start">
           <Link href="/" className="flex items-center gap-2">
@@ -24,7 +23,8 @@ export async function Navbar() {
           </Link>
         </div>
 
-        <nav className="hidden md:flex absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+        {/* Adjust the nav for smaller screens */}
+        <nav className="hidden md:flex">
           <ul className="flex items-center justify-center gap-8">
             <li className="hover:text-foreground/80 text-sm">
               <Link href="/#pricing">Pricing</Link>
