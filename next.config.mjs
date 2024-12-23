cc06568258302a8f011e624e477c17c848b3b760
Ayota <seculex4@gmail.com>
2024-12-23 10:19:09 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      'utfs.io',
-      'img.clerk.com',
-      'subdomain',
-    ],
-  },
-  reactStrictMode: false,
+    images: {
+        domains: [
+            'utfs.io',
+            'img.clerk.com',
+            'subdomain',
+        ],
+    },
+    reactStrictMode: false,
 };
 
 export default nextConfig;
